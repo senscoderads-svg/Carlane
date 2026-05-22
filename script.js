eef445c67f867702de0e86a1c5ea0aa5c9d02fe2
senscoderads-svg <senscoderads@gmail.com>
2026-05-22 14:14:27 +0000

    atualizar

--- script.js
+++
@@ -1,7 +0,0 @@
-const borboleta = document.getElementById('borboleta');
-
-document.addEventListener('mousemove', (e) => {
-    // Move a borboleta exatamente para as coordenadas do cursor
-    borboleta.style.left = e.clientX + 'px';
-    borboleta.style.top = e.clientY + 'px';
-});
